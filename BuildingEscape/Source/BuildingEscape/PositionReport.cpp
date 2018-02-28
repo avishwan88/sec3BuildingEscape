9aaea89abcb8dc65afd500c7ed18e8ba7f20aa6a
Avishlocal <aaron.narain@gmail.com>
2018-02-27 21:05:05 -0500

    position report files for position logging of two actors sharing actor component

---
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -0,0 +1,38 @@
+// Copyright Notice Arun Vish
+
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+#include "Math/TransformNonVectorized.h"
+#include "Math/Vector.h"
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectLocation = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s reporting at %s!"), *ObjectName, *ObjectLocation);
+	
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
