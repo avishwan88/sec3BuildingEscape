afa8fabe7dd9b120e1895d8ba52e94927ce3638f
Avishlocal <aaron.narain@gmail.com>
2018-03-12 21:11:56 -0400

    Blueprint game mode with pawn blueprint with grabber and reporting position and rotation

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,42 @@
+// Copyright Notice Arun Vish
+
+#include "Grabber.h"
+#include "Engine/World.h" // needed for UWorld type
+#include "GameFramework/PlayerController.h"
+
+#define OUT
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation);
+	UE_LOG(LogTemp, Warning, TEXT("Player at %s reporting for duty at %s!"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString());
+}
+
